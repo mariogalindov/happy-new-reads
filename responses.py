3be441480b605a8f16eeb5b0b3941e041e544ca6
Mario Alberto Galindo Valero <marioalbertogalindovalero@MacBook-Pro-de-Mario.local>
2017-12-12 20:57:06 -0600

    merge with mario

--- responses.py
+++ responses.py
@@ -1,2 +1,14 @@
 import requests
 from pymessenger.bot import Bot
+
+class Responses():
+	bot=""
+	def __init__(self,access_token):
+		bot = Bot(access_token)
+		self.bot = bot
+
+	def saluda(self, sender_id):
+		text = "HOLA :D"
+		self.bot.send_text_message(sender_id,text)
+
+
