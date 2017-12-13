3783e28c49234c7491a3657640c00721c9414a3c
Mario Alberto Galindo Valero <marioalbertogalindovalero@MacBook-Pro-de-Mario.local>
2017-12-12 20:56:50 -0600

    merge with mario

--- index.py
+++ index.py
@@ -6,8 +6,11 @@ from responses import Responses
 
 app = Flask(__name__)
 
-ACCESS_TOKEN = 'EAAEzHJeqzxsBABisf2HpZCD4EdudErzscxGQDHENCEFhJMkCxun8bT00w0BpZCukLDwwZCQGGbcEqp9QAJmaorp93HXb6Tl3HxNlR9wcbQVn37Eu5FM3Oft51gQnD4pZCl69dPD1hNobEcODokyw48subgkD0nFa1Bs4TwV9cgZDZD'
+ACCESS_TOKEN = 'EAAKHni1byYIBAOFXGbTIOmUGb59kK20zSNsZAiPYegFxV8YHTcaT43NXNPgVWeiCON7N6aE7BN4Q6mZBznBMoajFsi2g0k7GDXuiGqh6a45cTMF940CKhbLvfbXHPfj1v4VPA2i9NHzaiPazaFZATz688tOziWnfiUjo2qZBFAZDZD'
 VERIFY_TOKEN = 'one5udx4'
+
+bot = Responses(ACCESS_TOKEN)
+
 
 @app.route("/")
 def index():
@@ -15,6 +18,21 @@ def index():
 
 @app.route('/webhook', methods = ['GET', 'POST'])
 def webhook():
+	if request.method == 'POST':
+		message = request.json
+		for event in message["entry"]:
+			messaging = event["messaging"]
+			for event_message in messaging:
+				sender_id = event_message["sender"]["id"]
+		bot.saluda(sender_id)
+		return 'ok' #Este siempre se queda porque le responde un 200 a FB
+
+
+
+	elif request.method == 'GET':
+		if request.args.get('hub.verify_token') == VERIFY_TOKEN:
+			return request.args.get('hub.challenge')
+		return 'Verificar token'
 		
 
 if __name__ == "__main__":
