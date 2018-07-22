478343f8784f37736eda24bd8cc717fefb1c17d9
Mario Galindo <mariogalindo89@gmail.com>
2018-07-22 14:29:29 -0500

    indentation error

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ def greeting():
 				  }
 					]
 				}
-		URL="https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAKHni1byYIBAIailUcuuVrwNLqmsG7VGEK29I5erzE3gC4ZCaITZCwxZAHTqMcePZCg3Y0K8yJe2VXC80ZBNXVZBZASpZCpJrTS3dX78hbWxCbcpwAv3KxBgayN2o0fVmvCfdY6G9g4yiRh4f1jLbSJNbD7EVupxjsllHyh77wit0nQwYLgKiha"
-		send=requests.post(URL,json = JSON)
+			URL="https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAKHni1byYIBAIailUcuuVrwNLqmsG7VGEK29I5erzE3gC4ZCaITZCwxZAHTqMcePZCg3Y0K8yJe2VXC80ZBNXVZBZASpZCpJrTS3dX78hbWxCbcpwAv3KxBgayN2o0fVmvCfdY6G9g4yiRh4f1jLbSJNbD7EVupxjsllHyh77wit0nQwYLgKiha"
+			send=requests.post(URL,json = JSON)
 
 
 def webhook():
