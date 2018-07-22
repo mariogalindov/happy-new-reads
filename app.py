62ddee8f5a9d57a5ea18591c5869200fb000d3ac
Mario Galindo <mariogalindo89@gmail.com>
2018-07-22 15:21:16 -0500

    greeting json

--- app.py
+++ app.py
@@ -19,24 +19,22 @@ def index():
 
 @app.route('/webhook', methods = ['GET', 'POST'])
 
-def greeting():
-	JSON = {
-		"get_started":{
-   			"payload":"<Holaa>"
-  			},				
-		"greeting":[
-  				{
-			    "locale":"es_LA",
-			    "text":"Hola!"
-				  }, {
-				    "locale":"es_LA",
-				    "text":"El chatbot que te ayudará en tu camino a la lectura."
-				  }
-					]
-				}
-			URL="https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAKHni1byYIBAIailUcuuVrwNLqmsG7VGEK29I5erzE3gC4ZCaITZCwxZAHTqMcePZCg3Y0K8yJe2VXC80ZBNXVZBZASpZCpJrTS3dX78hbWxCbcpwAv3KxBgayN2o0fVmvCfdY6G9g4yiRh4f1jLbSJNbD7EVupxjsllHyh77wit0nQwYLgKiha"
-			send=requests.post(URL,json = JSON)
-
+URL="https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAKHni1byYIBAIailUcuuVrwNLqmsG7VGEK29I5erzE3gC4ZCaITZCwxZAHTqMcePZCg3Y0K8yJe2VXC80ZBNXVZBZASpZCpJrTS3dX78hbWxCbcpwAv3KxBgayN2o0fVmvCfdY6G9g4yiRh4f1jLbSJNbD7EVupxjsllHyh77wit0nQwYLgKiha"
+empezar_JSON= { 
+    "get_started":{
+    "payload":"<Empezar!>"
+  },
+    "greeting": [
+    {
+      "locale":"default",
+      "text":"Hello!" 
+    }, {
+      "locale":"en_US",
+      "text":"Timeless apparel for the masses."
+    }
+  ]
+}
+send = requests.post(URL, json=empezar_JSON)
 
 def webhook():
 	if request.method == 'POST':
